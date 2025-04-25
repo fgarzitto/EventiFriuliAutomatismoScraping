1cb9c050d51aee6555eef9fd5e0e5bd75b73c8bf
fgarzitto <fgarzitto@gmail.com>
2025-04-25 09:19:43 +0200

    Update test_google_sheet.py
    c

--- test_google_sheet.py
+++ test_google_sheet.py
@@ -7,20 +7,28 @@ def test_google_sheet_access():
     client_email = os.getenv("GSHEET_CLIENT_EMAIL")
     private_key = os.getenv("GSHEET_PRIVATE_KEY")  # Nessuna sostituzione necessaria
 
-    # Configura le credenziali
+    # Configura manualmente il dizionario delle credenziali
     credentials_info = {
         "type": "service_account",
-        "client_email": client_email,
+        "project_id": "EventiFriuli",  # Sostituisci con l'ID del tuo progetto
+        "private_key_id": "2ad6e92ed5bd78ebb61505057bc75ecb4130b6a6",  # Sostituisci con l'ID della chiave privata
         "private_key": private_key,
-        "token_uri": "https://oauth2.googleapis.com/token"
+        "client_email": client_email,
+        "client_id": "103136377669455790448",  # Sostituisci con l'ID del client
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{client_email}"
     }
+
+    # Autenticazione
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
     client = gspread.authorize(credentials)
 
     # Verifica l'accesso al Google Sheet
     try:
-        sheet_id = "1XuiL6v5EJAJDae5JwZBrA2Uu2zJ4YlUIYPCoK_0W6AI"  # Sostituisci con l'ID del tuo Google Sheet
+        sheet_id = "1XuiL6v5EJAJDae5JwZBrA2Uu2zJ4YlUIYPCoK_0W6AI"  # Sostituisci con l'ID del Google Sheet
         sheet = client.open_by_key(sheet_id)
         print(f"Accesso riuscito al Google Sheet: {sheet.title}")
     except Exception as e:
